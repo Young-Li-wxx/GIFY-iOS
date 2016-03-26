ddc05dec596084ee63a688153649695c26debb98
Louie Shum <xm.walfer@gmail.com>
2016-03-27 01:42:29 +0800

    fix bug of timestamp

--- GifKeyboard/GifKeyboard/ffmpeg/FFmpeg-iOS/GKFFmpeg.m
+++ GifKeyboard/GifKeyboard/ffmpeg/FFmpeg-iOS/GKFFmpeg.m
@@ -14,57 +14,13 @@
 #include "libavutil/frame.h"
 #include "libswscale/swscale.h"
 
-//static int open_codec_context(int *stream_idx,
-//                              AVFormatContext *fmt_ctx, enum AVMediaType type)
-//{
-//    int ret, stream_index;
-//    AVStream *st;
-//    AVCodecContext *dec_ctx = NULL;
-//    AVCodec *dec = NULL;
-//    AVDictionary *opts = NULL;
-//    
-//    ret = av_find_best_stream(fmt_ctx, type, -1, -1, NULL, 0);
-//    if (ret < 0) {
-//        fprintf(stderr, "Could not find %s stream in input file '%s'\n",
-//                av_get_media_type_string(type), "local files");
-//        return ret;
-//    } else {
-//        stream_index = ret;
-//        st = fmt_ctx->streams[stream_index];
-//
-//        /* find decoder for the stream */
-//        dec_ctx = st->codec;
-//        dec = avcodec_find_decoder(dec_ctx->codec_id);
-//        if (!dec) {
-//            fprintf(stderr, "Failed to find %s codec\n",
-//                    av_get_media_type_string(type));
-//                    return AVERROR(EINVAL);
-//        }
-//
-//        /* Init the decoders, with or without reference counting */
-//        av_dict_set(&opts, "refcounted_frames", "0", 0);
-//        if ((ret = avcodec_open2(dec_ctx, dec, &opts)) < 0) {
-//            fprintf(stderr, "Failed to open %s codec\n",
-//                    av_get_media_type_string(type));
-//            return ret;
-//        }
-//        *stream_idx = stream_index;
-//    }
-//    return 0;
-//}
-
 @interface GKFFmpeg(){
     AVFormatContext *avFormateCtx;
     AVStream *videoStream;
     AVCodecContext *videoDecCtx;
     AVCodec *avCodec;
     
-    int64_t *pArrayTimeStamp;
-    
     int video_stream_idx;
-    
-    NSInteger numberOfPackages;
-    int64_t videoDuration;
 }
 @property (nonatomic, strong) NSString *path;
 @end
@@ -103,17 +59,10 @@
         }
         
         video_stream_idx = -1;
-#if 0
-        for(int i=0; i<avFormateCtx->nb_streams; i++)
-            if(avFormateCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO){
-                video_stream_idx=i;
-                break;
-            }
-#else
-//        open_codec_context(&video_stream_idx, avFormateCtx, AVMEDIA_TYPE_VIDEO);
+
         if ((video_stream_idx = av_find_best_stream(avFormateCtx, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0)))
             return self;
-#endif
+
         if (video_stream_idx < 0)
             return self;
         
@@ -126,30 +75,6 @@
         if (avcodec_open2(videoDecCtx, avCodec, NULL) < 0)
             return self;
         
-        video_stream_idx = -1;
-        pArrayTimeStamp = malloc(200*sizeof(int64_t));
-        memset(pArrayTimeStamp, 0, sizeof(int64_t)*200);
-        int length = 200;
-        
-        AVPacket orig_pkt;
-        videoDuration = 0;
-        numberOfPackages = 0;
-        
-        while (av_read_frame(avFormateCtx, &orig_pkt) >= 0) {
-            numberOfPackages += 1;
-            
-            if (numberOfPackages>= length) {
-                length += 200;
-                pArrayTimeStamp = realloc(pArrayTimeStamp, length*sizeof(int64_t));
-                memset(&pArrayTimeStamp[length-200], 0, 200*sizeof(int64_t));
-            }
-            
-            pArrayTimeStamp[numberOfPackages] = videoDuration;
-            videoDuration += orig_pkt.duration;
-            
-            av_packet_unref(&orig_pkt);
-        }
-        
         av_dump_format(avFormateCtx, 0, [path UTF8String], 0);
     }
     return self;
@@ -160,82 +85,70 @@
     
     if (avFormateCtx)
         avformat_close_input(&avFormateCtx);
-
-    if (pArrayTimeStamp){
-        free(pArrayTimeStamp);
-        pArrayTimeStamp = NULL;
-    }
+}
+- (CGFloat)duration{
+    return videoStream->duration * videoStream->time_base.num / videoStream->time_base.den;
 }
 - (NSInteger)numberOfImages{
-    return numberOfPackages;
+    return [self duration] * 10;
+//    return videoStream->nb_frames;
+//    return numberOfPackages;
 }
 - (UIImage *)imageAtIndex:(NSInteger)index{
-    if (!videoStream || index<0 || index>numberOfPackages)
+    if (!videoStream || index<0 || index>[self numberOfImages])
         return nil;
     
-    int64_t seekTime = av_rescale_q(pArrayTimeStamp[index], AV_TIME_BASE_Q, videoStream->time_base);
-    int64_t seekStreamDuration = videoStream->duration;
-    int flags = AVSEEK_FLAG_BACKWARD;
-    if (seekTime<0 || seekTime>=seekStreamDuration) {
-        return nil;
-    }
-    
-    int ret = av_seek_frame(avFormateCtx, video_stream_idx, seekTime, flags);
-    if (ret<0)
+    if (![self seekToTime:index/10.0])
         return nil;
     
+    UIImage *imageDecode = nil;
+    AVFrame *frame = av_frame_alloc();
     AVPacket pkt;
+    if([self stepFrame:frame withPacket:&pkt])
+        imageDecode = [self convertFrame:frame];
+    
+    av_frame_free(&frame);
+    av_packet_unref(&pkt);
+    return imageDecode;
+}
+
+- (BOOL)seekToFrame:(int64_t)targetFrame{
+    int ret = avformat_seek_file(avFormateCtx, video_stream_idx, targetFrame, targetFrame, targetFrame, AVSEEK_FLAG_FRAME);
+    if (ret<0)
+        return NO;
+    avcodec_flush_buffers(videoDecCtx);
+    return YES;
+}
+- (BOOL)seekToTime:(double)seconds{
+    AVRational timeBase = videoStream->time_base;
+    int64_t targetFrame = (int64_t)((double)timeBase.den / timeBase.num * seconds);
+    
+    int ret = avformat_seek_file(avFormateCtx, video_stream_idx, targetFrame, targetFrame, targetFrame, AVSEEK_FLAG_FRAME);
+    if (ret<0)
+        return NO;
+    avcodec_flush_buffers(videoDecCtx);
+    return YES;
+}
+- (BOOL)stepFrame:(AVFrame *)frame withPacket:(AVPacket *)pkt{
+    int frameFinished=0;
+    if (!frame)
+        return NO;
+    
     /* initialize packet, set data to NULL, let the demuxer fill it */
-    av_init_packet(&pkt);
-    pkt.data = NULL;
-    pkt.size = 0;
+    av_init_packet(pkt);
+    pkt->data = NULL;
+    pkt->size = 0;
     
-    if (av_read_frame(avFormateCtx, &pkt) >= 0) {
-        int got_picture = 1;
-        AVFrame *frame = av_frame_alloc();
-        
-        UIImage *imageDecode = nil;
-        
-        int ret = avcodec_decode_video2(videoStream->codec, frame, &got_picture, &pkt);
-        if (ret > 0) {
-            imageDecode = [self convertFrame:frame];
+    while(!frameFinished && av_read_frame(avFormateCtx, pkt)>=0) {
+        // Is this a packet from the video stream?
+        if(pkt->stream_index==video_stream_idx) {
+            // Decode video frame
+            avcodec_decode_video2(videoStream->codec, frame, &frameFinished, pkt);
         }
-        
-        av_frame_free(&frame);
-        av_packet_unref(&pkt);
-        return imageDecode;
     }
-    
-    return nil;
+    return frameFinished!=0;
 }
 - (UIImage *)convertFrame:(AVFrame *)pFrame{
-    /* float with = pCodecCtx->width;
-       float height = pCodecCtx->height;*/
-    
-//    if (pFrame->data[0]) {
-//        struct SwsContext * scxt =sws_getContext(with,height,PIX_FMT_YUV420P,with,height,PIX_FMT_RGBA,SWS_POINT,NULL,NULL,NULL);
-//        if (scxt == NULL) {
-//            return nil;
-//        }
-//        
-//        avpicture_alloc(&picture,PIX_FMT_RGBA, with, height);
-//        sws_scale (scxt, (const uint8_t **)pFrame->data, pFrame->linesize, 0,height,picture.data, picture.linesize);
-//        CGBitmapInfo bitmapInfo =kCGBitmapByteOrderDefault;
-//        CFDataRef data = CFDataCreateWithBytesNoCopy(kCFAllocatorDefault,picture.data[0],picture.linesize[0]*height,kCFAllocatorNull);
-//        CGDataProviderRef provider =CGDataProviderCreateWithCFData(data);
-//        CGColorSpaceRef colorSpace =CGColorSpaceCreateDeviceRGB();
-//        CGImageRef cgImage = CGImageCreate(with,height,
-//                                           8,24,picture.linesize[0],
-//                                           colorSpace,bitmapInfo,provider,NULL,NO,kCGRenderingIntentDefault);
-//        CGColorSpaceRelease(colorSpace);
-//        UIImage *image = [UIImageimageWithCGImage:cgImage];
-//        CGImageRelease(cgImage);
-//        CGDataProviderRelease(provider);
-//        CFRelease(data);
-//        return image;
-//    }
-//    return nil;
-    
     float width = videoDecCtx->width;
     float height = videoDecCtx->height;
     
